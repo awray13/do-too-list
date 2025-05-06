b6e915ef2ce0df75d853248333be8f6a0595ca4d
Ashton Wray <aricks1986@gmail.com>
2025-05-06 18:55:58 -0400

    initial commit after adding gitignore file

---
+++ DoTooList/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace DoTooList
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Override the CreateWindow method to return a custom window instance. This is the starting point for the application.
+        /// </summary>
+        /// <param name="activationState"></param> 
+        /// <returns></returns>
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ DoTooList/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DoTooList
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
